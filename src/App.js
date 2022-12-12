0f32effd54d47afd9af8795c8d66441b022569e0
newExpand <magin0auto@gmail.com>
2022-12-12 23:56:37 +0900

    docs: 요구 사항 정리 feat : 시작 문장 출력 및 입출력 기능 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  constructor() {
+    this.randomNum = new RandomCount();
+    this.game = new GameCourse(this.randomNum);
+  }
+
+  play() {
+    this.game.startText();
+    this.game.writeInput();
+  }
+}
+
+class RandomCount {
+  getCount () {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    // console.log(computer)
+    return computer;
+  }
+}
+
+class GameCourse {
+  constructor(randomNum) {
+    this.randomNum = randomNum;
+  }
+
+  startText() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    // this.randomNum.getCount();
+  }
+
+  writeInput() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (answer) => {
+      MissionUtils.Console.print(answer);
+      
+    });
+  }
 }
 
 module.exports = App;
