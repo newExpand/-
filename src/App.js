4559494b3eca9db957126f1de85147585c3bc315
newExpand <magin0auto@gmail.com>
2022-12-14 23:40:44 +0900

    feat: 기능 4,5,6 및 에러 케이스 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-
 class App {
   constructor() {
-    this.randomNum = new RandomCount();
-    this.game = new GameCourse(this.randomNum);
+    this.game = new GameCourse();
   }
 
   play() {
     this.game.startText();
-    this.game.writeInput();
+    this.game.repeatContext();
   }
 }
 
 class RandomCount {
-  get getCount () {
+  get getCount() {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -29,16 +27,50 @@ class RandomCount {
 class GameCourse {
   constructor(randomNum) {
     this.randomNum = randomNum;
+    this.victory = false;
   }
 
   startText() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
-  writeInput() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (answer) => {
-      MissionUtils.Console.print(this.compareValue(answer, this.randomNum.getCount));
-      // return this.writeInput();
+  repeatContext() {
+    const computerAnswer = new RandomCount();
+    this.writeInput(computerAnswer.getCount);
+  }
+
+  userInputErrorHandler(answer) {
+    try {
+      if (Number.isNaN(answer)) {
+        throw "숫자가 아닌값이 포함되있습니다";
+      }
+      if (answer.includes(" ")) {
+        throw "공백이 포함되있습니다";
+      }
+      if (/[^1-9]/g.test(answer)) {
+        throw "1~9가 아닌 수가 포함되있습니다.";
+      }
+      if (new Set(answer).size !== 3) {
+        throw "중복된 숫자가 포함되있습니다";
+      }
+      if (answer.length !== 3) {
+        throw "3자리의 수를 입력해주세요";
+      }
+    } catch (err) {
+      MissionUtils.Console.close();
+      throw new Error(err);
+    }
+
+
+    return true;
+  }
+
+  writeInput(computerAnswer) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
+      this.userInputErrorHandler(answer);
+      MissionUtils.Console.print(this.compareValue(answer, computerAnswer));
+      if (this.victory) {return this.reset()};
+      return this.writeInput(computerAnswer);
     });
   }
 
@@ -68,9 +100,37 @@ class GameCourse {
 
   compareValue(answer, randomNum) {
     let strike = this.strike(answer, randomNum);
-    let ball = this.ball(answer, randomNum)
+    let ball = this.ball(answer, randomNum);
 
-    console.log(strike, ball)
+    console.log('answer =>' , answer, 'randomNum =>', randomNum)
+    if (strike === 0 && ball === 0) {
+      return `낫싱`;
+    }
+    if (strike === 0 && ball !== 0) {
+      return `${ball}볼`;
+    }
+    if (ball === 0 && strike !== 0 && strike !== 3) {
+      return `${strike}스트라이크`;
+    }
+    if (strike === 3) {
+      this.victory = true;
+      return `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+    }
+    return `${ball}볼 ${strike}스트라이크`;
+  }
+
+  reset() {
+    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",(input) => {
+        if (input === "1") {
+          this.victory = false;
+          return this.repeatContext();
+        }
+
+        if (input === "2") {
+          return MissionUtils.Console.close();
+        }
+      }
+    );
   }
 }
 
